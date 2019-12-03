2a2ba825eb0437a90775a4b74108e4007e550f89
Ryan Mobley <ryan.mobley94@gmail.com>
2019-12-03 14:29:09 -0600

    good ol heroku

--- server.js
+++ server.js
@@ -9,16 +9,16 @@ const app = express();
 
 
 
-app.use(express.static(path.join(__dirname, 'client/public')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
 app.use(routes);
 
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./client/public/index.html"));
-  });
+  // app.get("*", function(req, res) {
+  //   res.sendFile(path.join(__dirname, "./client/public/index.html"));
+  // });
   
   
   mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/rocksFall");
